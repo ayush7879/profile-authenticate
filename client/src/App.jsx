67abe6c5a1f07523f60d7d5144d7bde3372a1f49
ayush7879 <ayushpersonal2003@gmail.com>
2023-12-13 20:34:29 +0530

    pages added

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import {BrowserRouter,Routes,Route}from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+import Profile from "./pages/Profile"
+export default function App() {
+  return (
+    <BrowserRouter>
+<Routes>
+  <Route path="/" element={ <Home/>} />
+  <Route path="/about" element={ <About/>} />
+  <Route path="/sign-in" element={ <SignIn/>} />
+  <Route path="/sign-up" element={ <SignUp/>} />
+  <Route path="/profile" element={ <Profile/>} /> 
+
+</Routes>
+    </BrowserRouter>
+    // <div className='text-red-600'>App</div>
+  )
+}
