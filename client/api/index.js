8228f05b0148910cee88bfc2adef9848923e734c
ayush7879 <ayushpersonal2003@gmail.com>
2023-12-15 22:02:47 +0530

    update signup api route

--- client/api/index.js
+++ client/api/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 const app=express();
-import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userroute from "./route/user.route.js"
 import authroute from "./route/auth.route.js"
+import dotenv from "dotenv";
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to MongoDB");
@@ -11,7 +11,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 .catch((err)=>{
     console.log(err);
 })
-
+app.use(express.json());
 app.listen(3000,()=>{
     console.log("Server is working on port 3000");
 })
