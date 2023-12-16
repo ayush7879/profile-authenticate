1ff8c8777fa337b4085ec4b6b796fb756b5a70df
ayush7879 <ayushpersonal2003@gmail.com>
2023-12-16 10:46:52 +0530

    complete signup page UI  with tailwind

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+    <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
+    <form className='flex flex-col gap-4' >
+<input type="text"placeholder='Username'id='username' className='bg-slate-100 rounded-lg p-3' />
+<input type="email"placeholder='Email'id='email' className='bg-slate-100 rounded-lg p-3' />
+<input type="password"placeholder='Password'id='password' className='bg-slate-100 rounded-lg p-3' />
+<button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>SignUp</button>
+    </form>
+    <div className='flex gap-1 mt-5'>
+<p>Have an account?</p>
+<Link to="/sign-in">
+<span className='text-blue-600'>Signin</span></Link>
+    </div>
+    </div>
   )
 }
